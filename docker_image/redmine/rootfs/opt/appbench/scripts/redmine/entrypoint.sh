b71c9041662643a777746bed7e988f3caa1cfbfc
Yu Kimura <awegl.teo@gmail.com>
2021-07-10 12:51:24 +0900

    Initial Implementation: rubymine starting with docker-compose

--- docker_image/redmine/rootfs/opt/appbench/scripts/redmine/entrypoint.sh
+++ docker_image/redmine/rootfs/opt/appbench/scripts/redmine/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # start PostgreSQL
-# /etc/init.d/postgresql start
+/etc/init.d/postgresql start
 
 # create dartabase
-# sudo -E -u redmine RAILS_ENV=production bundle exec rails db:create db:migrate
+sudo -E -u redmine RAILS_ENV=production bundle exec rails db:create db:migrate
 
 # start rails application
-# sudo -E -u redmine RAILS_ENV=production bundle exec unicorn_rails -c config/unicorn.rb
+sudo -E -u redmine RAILS_ENV=production bundle exec unicorn_rails -c config/unicorn.rb
