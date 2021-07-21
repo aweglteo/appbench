b56880f73128633a866cc0c303362ebeb826c7d9
Yu Kimura <awegl.teo@gmail.com>
2021-07-21 17:35:14 +0900

    update

--- docker_image/redmine/rootfs/tmp/entrypoint.sh
+++ docker_image/redmine/rootfs/tmp/entrypoint.sh
@@ -2,25 +2,13 @@
 
 # start PostgreSQL
 /etc/init.d/postgresql start
-
-
-gem update --system
-gem install facter
-gem install stackprof
-
-bundle config set --local without 'development test'
-bundle install --jobs 3 --retry 5 --path ./vendor/bundle
-bundle config set --local without 'development test' && bundle clean --force
-
-# create dartabase
-RAILS_ENV=production bundle exec rails db:create db:migrate
-
-# TEMP
-chown -R redmine result/ tmp/
+sleep 5
 
 if [ $APPBENCH_TARGET = "throughput" ]; then
   ruby appbench/bench.rb
 elif [ $APPBENCH_TARGET = "rprof" ]; then
   # run the rpro
   ruby appbench/rprof.rb
+elif [ -z $APPBENCH_TARGET ]; then
+  bundle exec puma -w 3
 fi
